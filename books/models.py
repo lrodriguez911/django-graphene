2264d46ed734ca93315ef0915875649eeb074210
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-08-10 22:05:51 -0300

    add new project

---
+++ books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField('Author')
+    publisher = models.ForeignKey('Publisher', on_delete=models.CASCADE)
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
