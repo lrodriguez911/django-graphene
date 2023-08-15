5d8a22711c5197abacc3d4ab0620ffd0a2105a32
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-08-14 23:52:57 -0300

    add mutacion and create book mutation

--- core/schema.py
+++ core/schema.py
@@ -7,23 +7,48 @@ class BookType(DjangoObjectType):
     class Meta:
         model = Book
         fields = ("id", "title", "description", "price", "author")
+
 class AuthorType(DjangoObjectType):
     class Meta:
         model = Author
-        fields = ("first_name", "last_name", "id")
+        fields = ("id", "first_name", "last_name", "books")
+        
+
+class CreateBookMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String()
+        description = graphene.String()
+        price = graphene.Float()
+        author = graphene.ID()
+
+    book = graphene.Field(BookType)
+
+    def mutate(self, info, title, description, price, author):
+        book = Book(title=title, description=description, price=price, author=author)
+        book.save()
+        return CreateBookMutation(book=book)
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hi!")
     books = graphene.List(BookType)
-    author = graphene.String(AuthorType)
+    book = graphene.Field(BookType, id=graphene.ID())
+    authors = graphene.List(AuthorType)
+    author = graphene.Field(AuthorType, id=graphene.ID())
 
     def resolve_books(self, info):
         return Book.objects.all()
+    
+    def resolve_book(self, info, id):
+        return Book.objects.get(pk=id)
 
-    def resolve_author(self, info):
+    def resolve_authors(self, info):
         return Author.objects.all()
     
     """ def resolve_hello(self, info):
         return f'{self}' """
+    
+class Mutation(graphene.ObjectType):
+    create_book = CreateBookMutation.Field()       
+    create_author = graphene.Field(AuthorType, first_name=graphene.String(), last_name=graphene.String())
 
 schema = graphene.Schema(query=Query)
